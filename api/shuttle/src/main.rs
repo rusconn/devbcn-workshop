85a6d6c72f81297ba07743425537ae5cfda765d8
rusconn <rusconn3@gmail.com>
2023-08-07 17:05:13 +0900

    added instrumentation

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -10,6 +10,8 @@ async fn hello_world() -> &'static str {
 
 #[get("/version")]
 async fn version(db: actix_web::web::Data<sqlx::PgPool>) -> String {
+    tracing::info!("Getting version");
+
     let result = sqlx::query_scalar("SELECT version();")
         .fetch_one(db.get_ref())
         .await;

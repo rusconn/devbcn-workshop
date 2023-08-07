81f9548b16f4a2bb6be6e24f7fecf6de8fcd12ed
rusconn <rusconn3@gmail.com>
2023-08-07 21:52:03 +0900

    feat: add films endpoints

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -14,7 +14,9 @@ async fn actix_web(
     let pool = actix_web::web::Data::new(pool);
 
     let config = move |cfg: &mut ServiceConfig| {
-        cfg.app_data(pool).configure(api_lib::health::service);
+        cfg.app_data(pool)
+            .configure(api_lib::films::service)
+            .configure(api_lib::health::service);
     };
 
     Ok(config.into())

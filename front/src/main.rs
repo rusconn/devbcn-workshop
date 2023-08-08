db4195c02289aedbb6933621735b1858e29134b0
rusconn <rusconn3@gmail.com>
2023-08-08 23:54:53 +0900

    add header and footer

--- front/src/main.rs
+++ front/src/main.rs
@@ -1,3 +1,21 @@
+#![allow(non_snake_case)]
+mod components;
+
+use dioxus::prelude::*;
+
+use components::{Footer, Header};
+
 fn main() {
-    println!("Hello, world!");
+    wasm_logger::init(wasm_logger::Config::default().module_prefix("front"));
+    dioxus_web::launch(App);
+}
+
+fn App(cx: Scope) -> Element {
+    cx.render(rsx! {
+        main { class: "relative z-0 bg-blue-100 w-screen h-auto min-h-screen flex flex-col justify-start items-stretch",
+            Header {}
+            section { class: "md:container md:mx-auto md:py-8 flex-1" }
+            Footer {}
+        }
+    })
 }

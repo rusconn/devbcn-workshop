abe0378bc5ea1fe831335cc2ecc458d5fe41198c
rusconn <rusconn3@gmail.com>
2023-08-09 01:20:52 +0900

    add global states

--- front/src/main.rs
+++ front/src/main.rs
@@ -5,6 +5,7 @@ mod models;
 use dioxus::prelude::*;
 
 use components::{FilmModal, Footer, Header};
+use models::FilmModalVisibility;
 
 fn main() {
     wasm_logger::init(wasm_logger::Config::default().module_prefix("front"));
@@ -12,12 +13,21 @@ fn main() {
 }
 
 fn App(cx: Scope) -> Element {
+    use_shared_state_provider(cx, || FilmModalVisibility(false));
+
+    let is_modal_visible = use_shared_state::<FilmModalVisibility>(cx).unwrap();
+
     cx.render(rsx! {
         main { class: "relative z-0 bg-blue-100 w-screen h-auto min-h-screen flex flex-col justify-start items-stretch",
             Header {}
             section { class: "md:container md:mx-auto md:py-8 flex-1" }
             Footer {}
-            FilmModal { on_create_or_update: move |_| {}, on_cancel: move |_| {} }
+            FilmModal {
+                on_create_or_update: move |_| {},
+                on_cancel: move |_| {
+                    is_modal_visible.write().0 = false;
+                }
+            }
         }
     })
 }

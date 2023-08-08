3d415ba34720c09806bbfdc6ad66790a96c91308
rusconn <rusconn3@gmail.com>
2023-08-09 00:45:53 +0900

    add components and models

--- front/src/main.rs
+++ front/src/main.rs
@@ -1,9 +1,10 @@
 #![allow(non_snake_case)]
 mod components;
+mod models;
 
 use dioxus::prelude::*;
 
-use components::{Footer, Header};
+use components::{FilmModal, Footer, Header};
 
 fn main() {
     wasm_logger::init(wasm_logger::Config::default().module_prefix("front"));
@@ -16,6 +17,7 @@ fn App(cx: Scope) -> Element {
             Header {}
             section { class: "md:container md:mx-auto md:py-8 flex-1" }
             Footer {}
+            FilmModal { on_create_or_update: move |_| {}, on_cancel: move |_| {} }
         }
     })
 }

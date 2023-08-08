1f5bfc3a6df9b26fbc398465bc3b416564775049
rusconn <rusconn3@gmail.com>
2023-08-09 08:05:08 +0900

    add effects

--- front/src/main.rs
+++ front/src/main.rs
@@ -13,6 +13,27 @@ fn main() {
     dioxus_web::launch(App);
 }
 
+const API_ENDPOINT: &str = "api/v1";
+
+fn films_endpoint() -> String {
+    let window = web_sys::window().expect("no global `window` exists");
+    let location = window.location();
+    let host = location.host().expect("should have a host");
+    let protocol = location.protocol().expect("should have a protocol");
+    let endpoint = format!("{protocol}//{host}/{API_ENDPOINT}");
+    format!("{endpoint}/films")
+}
+
+async fn get_films() -> Vec<Film> {
+    log::info!("Fetching films from {}", films_endpoint());
+    reqwest::get(&films_endpoint())
+        .await
+        .unwrap()
+        .json::<Vec<Film>>()
+        .await
+        .unwrap()
+}
+
 fn App(cx: Scope) -> Element {
     use_shared_state_provider(cx, || FilmModalVisibility(false));
 
@@ -20,6 +41,19 @@ fn App(cx: Scope) -> Element {
     let films = use_state::<Option<Vec<Film>>>(cx, || None);
     let selected_film = use_state::<Option<Film>>(cx, || None);
     let force_get_films = use_state(cx, || ());
+
+    {
+        let films = films.clone();
+
+        use_effect(cx, force_get_films, |_| async move {
+            let existing_films = get_films().await;
+            if existing_films.is_empty() {
+                films.set(None);
+            } else {
+                films.set(Some(existing_films));
+            }
+        });
+    }
 
     cx.render(rsx! {
         main { class: "relative z-0 bg-blue-100 w-screen h-auto min-h-screen flex flex-col justify-start items-stretch",
